6cddc397cdb1bb0f2b703ac0305a48f286c4a7ac
mbouadeus <mbouadeus@gmail.com>
2020-07-22 20:41:39 +0000

    Add time pickers.

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -4,9 +4,15 @@ document.addEventListener("DOMContentLoaded", function(){
 
 function init() {
   initializeDatePickers();
+  initializeTimePickers();
 }
 
 function initializeDatePickers() {
   let elems = document.querySelectorAll('.datepicker');
   M.Datepicker.init(elems, {});
+}
+
+function initializeTimePickers() {
+  let elems = document.querySelectorAll('.timepicker');
+  M.Timepicker.init(elems, {});
 }
