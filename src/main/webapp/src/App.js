28534d16f21fc88cc820ca7555ee2c1d4004ac0c
mbouadeus <mbouadeus@gmail.com>
2020-07-28 22:43:07 +0000

    Removed unused files and personalized for our app

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { ReactComponent as Logo } from './assets/logo.svg';
 import Routes from './Routes';
 
-import Navbar from './components/Navbar';
+import Navbar from './constants/Navbar';
 
 class App extends Component {
   state = {
