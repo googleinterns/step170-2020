e876043024e81ea24cb91c7d3ba8f64b6a406cc2
bhargaviks <bhargavi.kumar2012@gmail.com>
2020-08-01 00:51:15 +0000

    added bootstrap and popper to app.js and npm packages for that

--- src/main/webapp/src/index.js
+++ src/main/webapp/src/index.js
@@ -5,6 +5,10 @@ import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import App from "./App";
 
+// Import Bootstrap CSS and JS
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render( <App /> , document.getElementById('root'));
