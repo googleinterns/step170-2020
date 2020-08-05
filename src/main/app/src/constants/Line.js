b5e8d3412b483793d35b6e7c442ff045b3c9d021
bhargaviks <bhargavi.kumar2012@gmail.com>
2020-08-05 00:28:30 +0000

    indented spaces

--- src/main/app/src/constants/Line.js
+++ src/main/app/src/constants/Line.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Line = ({ color }) => (
-    <hr
-        style={{
-            color: color,
-            backgroundColor: color,
-            height: 5,
-            marginTop: 50,
-            marginBottom: 50
-        }}
-    />
+const Line = ({color}) => (
+  <hr
+    style={{
+      color: color,
+      backgroundColor: color,
+      height: 5,
+      marginTop: 50,
+      marginBottom: 50
+    }}
+  />
 );
 
 export default Line;
