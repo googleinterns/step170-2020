d63b3ae21f01cbbf2d47e6ee6dffda136088c510
Bhargavi Kumar Sundaresan <43729861+bhargaviks@users.noreply.github.com>
2020-08-05 10:32:56 -0700

    Update keys.js
    Removed extra space

--- src/main/app/src/constants/keys.js
+++ src/main/app/src/constants/keys.js
@@ -4,7 +4,6 @@ const info = [{
       airtable_api_key_helpPage: "keyvEcGAXuzDAVmVb",
       clientID:"298755462-7fnqhho2db1b0mb5i6o7irgg8v7om76g.apps.googleusercontent.com",
       Airtable_URL:"https://api.airtable.com/v0/appun5As9N6CFqu4H/Resources?api_key=keyvEcGAXuzDAVmVb"
-
 }];
 
 export default info;
