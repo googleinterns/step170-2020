2466f565c5ef0ce3bc686634057d3fb81a5966e5
trinitydonohugh <trinitydonohugh@gmail.com>
2020-08-10 19:52:50 +0000

    Added changes to cards to simplfy the code, as per Steve's advice.

--- src/main/app/src/constants/ArticleCard.js
+++ src/main/app/src/constants/ArticleCard.js
@@ -3,21 +3,21 @@ import { useStyles } from '../hooks/useStyles';
 import { Button, Card, CardActionArea, CardActions, CardContent, Typography, Box } from '@material-ui/core';
 
 // This is a stateless, functional React component used to render each resource in a card format. 
-const ArticleCard = ({ activityKey, title, url, description, updateActivity, publisher }) => {
+const ArticleCard = ({ data, updateActivity }) => {
   const classes = useStyles();
 
-  if (!title) return <div />
+  if (!data.title) return <div />
   return (
     <Card className="m-3 classes.root" variant="outlined">
-      <CardActionArea href={url}>
+      <CardActionArea href={data.url}>
         <CardContent>
-          <Typography className={classes.title} color="textSecondary" gutterBottom>{publisher}</Typography>
-          <Typography gutterBottom variant="h5" component="h2">{title}</Typography>
-          <Typography className={classes.pos} color="textSecondary" component="p">{description}</Typography>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>{data.publisher}</Typography>
+          <Typography gutterBottom variant="h5" component="h2">{data.title}</Typography>
+          <Typography className={classes.pos} color="textSecondary" component="p">{data.description}</Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" href="../schedule-activity" onClick= {() => updateActivity({activityKey: activityKey, title: title})}>
+        <Button size="small" href="../schedule-activity" onClick= {() => updateActivity({activityKey: data.key, title: data.title})}>
           Schedule event
         </Button>
       </CardActions>
--- src/main/app/src/constants/GameCard.js
+++ src/main/app/src/constants/GameCard.js
@@ -3,21 +3,21 @@ import { useStyles } from '../hooks/useStyles';
 import { Button, Card, CardActionArea, CardActions, CardContent, Typography, Box } from '@material-ui/core';
 
 // This is a stateless, functional React component used to render each resource in a card format. 
-const GameCard = ({ activityKey, title, url, description, updateActivity }) => {
+const GameCard = ({ data, updateActivity }) => {
   const classes = useStyles();
 
-  if (!title) return <div />
+  if (!data.title) return <div />
   return (
     <Card className="m-3 classes.root" variant="outlined">
-      <CardActionArea href={url}>
+      <CardActionArea href={data.url}>
         <CardContent>
           <Typography className={classes.title} color="textSecondary" gutterBottom>Game</Typography>
-          <Typography gutterBottom variant="h5" component="h2">{title}</Typography>
-          <Typography className={classes.pos} color="textSecondary" component="p">{description}</Typography>
+          <Typography gutterBottom variant="h5" component="h2">{data.title}</Typography>
+          <Typography className={classes.pos} color="textSecondary" component="p">{data.notes}</Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" href="../schedule-activity" onClick= {() => updateActivity({activityKey: activityKey, title: title})}>
+        <Button size="small" href="../schedule-activity" onClick= {() => updateActivity({activityKey: data.key, title: data.title})}>
           Schedule event
         </Button>
       </CardActions>
