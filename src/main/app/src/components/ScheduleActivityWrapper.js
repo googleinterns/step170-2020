e61d760f49765b6e896e94cd8b20b90753deb737
mbouadeus <mbouadeus@gmail.com>
2020-08-11 23:04:23 +0000

    Add todo message.

--- src/main/app/src/components/ScheduleActivityWrapper.js
+++ src/main/app/src/components/ScheduleActivityWrapper.js
@@ -5,6 +5,7 @@ import ScheduleActivityPage from '../pages/ScheduleActivityPage';
     It holds the state of the random activities generated to prevent
     unnecessary rerenderes. */
 const ScheduleActivityWrapper = props => {
+  // Todo: Move randomActivities state from the schedule activity page to here.
   const [randomActivities, updateRandomActivities] = React.useState([]);
   return (
     <ScheduleActivityPage {...props} />
