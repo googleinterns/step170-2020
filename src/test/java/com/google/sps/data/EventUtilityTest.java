c8f864da70fa98c193c88b13538aff3b6059b799
mbouadeus <mbouadeus@gmail.com>
2020-08-13 20:25:05 +0000

    Create test for retrieving activities of type game.

--- src/test/java/com/google/sps/data/EventUtilityTest.java
+++ src/test/java/com/google/sps/data/EventUtilityTest.java
@@ -14,26 +14,94 @@
 
 package com.google.sps;
 
-import org.junit.Assert;
 import org.junit.Test;
-import org.junity.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.sps.data.EventUtility;
+import com.google.sps.data.Activity;
+
+import com.google.appengine.api.datastore.EntityNotFoundException;
+
+import java.util.Map;
+import java.util.HashMap;
 
 @RunWith(JUnit4.class)
 public final class EventUtilityTest {
 
-  @Ignore
-  private static DatastoreService getDatastoreMock() {
-    // Todo: use mockito to generate a datastore mock.
+  private static DatastoreService getDatastoreMock(Entity entity) throws EntityNotFoundException {
+    // Use mockito to generate a datastore mock.
+    DatastoreService datastoreMock = mock(DatastoreService.class);
+
+    // Mock response for getting entity from datastore
+    when(datastoreMock.get(any(Key.class))).thenReturn(entity);
+
+    return datastoreMock;
+  }
+
+  private static Entity getEntityMock(String type, Map<String, String> properties) throws EntityNotFoundException {
+    // Use mockito to generate an entity mock of the specified type.
+    Entity entityMock = mock(Entity.class);
+
+    // Set return value for entity kind
+    when(entityMock.getKind()).thenReturn(type);
+
+    // Set return value for each entity property
+    for (String key : properties.keySet())
+      when(entityMock.getProperty(key)).thenReturn(properties.get(key));
+
+    return entityMock;
+  }
+
+  private static Key getKeyMock() throws EntityNotFoundException {
+    // Use mockito to generate a mock key.
+    Key keyMock = mock(Key.class);
+    return keyMock;
+  }
+
+  private static Activity getTestActivity(String type, String keyStr, Map<String, String> properties) throws EntityNotFoundException {
+    // Create a mock key
+    Key keyMock = getKeyMock();
+
+    // Create a mock entity of type game
+    Entity entityMock = getEntityMock(type, properties);
+
+    // Create datastore mock that returns specified entity
+    DatastoreService datastoreMock = getDatastoreMock(entityMock);
+
+    // Get Activity
+    EventUtility eventUtility = new EventUtility(datastoreMock);
+    Activity activity = eventUtility.getActivity(keyMock, keyStr);
+
+    return activity;
   }
 
   @Test
-  public void testGetActivity() {
-    // Todo: test getActivity method.
+  public void testGetGameActivity() throws EntityNotFoundException {
+    String type = "game";
+    String keyStr = "key";
+
+    // Create map of game entity properties
+    Map<String, String> properties = new HashMap<>();
+    String title = "game title";
+    String url = "game url";
+    properties.put("title", title);
+    properties.put("url", url);
+
+    Activity activity = getTestActivity(type, keyStr, properties);
+
+    // Check if values are the same
+    assertEquals(keyStr, activity.getKey());
+    assertEquals(Activity.Category.valueOf("GAMES"), activity.getCategory());
+    assertEquals(title, activity.getTitle());
+    assertEquals(url, activity.getUrl());
   }
 }
