55a34048c83d9d59b8303ce6933ae6bfeeea7942
bhargaviks <bhargavi.kumar2012@gmail.com>
2020-08-13 22:01:35 +0000

    added more tests to doPut

--- src/test/java/com/google/sps/servlets/getArticlesServletTest.java
+++ src/test/java/com/google/sps/servlets/getArticlesServletTest.java
@@ -1,5 +1,6 @@
 package com.google.sps.servlets;
-
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.*;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
@@ -100,30 +101,25 @@ public class getArticlesServletTest {
   }
 
   @Test
-  public void doPut() throws Exception {
+  public void gettingArticlesFromApiTest() throws Exception {
     HttpServletRequest request = mock(HttpServletRequest.class);
     HttpServletResponse response = mock(HttpServletResponse.class);
 
-    //These lines mock response.getWriter()
-    StringWriter stringWriter = new StringWriter();
-    PrintWriter writer = new PrintWriter(stringWriter);
-    when(response.getWriter()).thenReturn(writer);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    List<Article> articles = new ArrayList<>();
 
+    // Make sure that the size of articles is initially zero before calling the servlet.
+    assertEquals(0,articles.size());
+
+    //calling the servlet
     new getArticlesServlet().doPut(request, response);
 
     //verify that request was never accessed
     verify(request, never()).getParameter(anyString());
 
-    //flush the writer response
-    writer.flush();
-
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-
     Query query = new Query("Article");
     PreparedQuery results = datastore.prepare(query);
-
-    List<Article> articles = new ArrayList<>();
-
+    
     for (Entity entity : results.asIterable()) {
       String entityKey = KeyFactory.createKeyString(entity.getKey().getKind(), entity.getKey().getId());
       String publisher = (String) entity.getProperty("publisher");
@@ -137,7 +133,50 @@ public class getArticlesServletTest {
       articles.add(newArticle);
     }
 
-    assertNotNull(articles);
+    // After calling the API, the size of articles can never be zero. 
+    assertNotEquals(0,articles.size());
 
+  }
+
+  @Test
+  public void deleteResultsOfQueryFromDatastoreTest() throws Exception {
+
+    //mocking response and request to pass to servlet.
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpServletResponse response = mock(HttpServletResponse.class);
+
+    DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+
+    Entity articleEntity = new Entity("Article");
+    articleEntity.setProperty("publisher", "testPublisher");
+    articleEntity.setProperty("author", "testAuthor");
+    articleEntity.setProperty("title", "testTitle");
+    articleEntity.setProperty("description", "testDescription");
+    articleEntity.setProperty("url", "testUrl");
+    articleEntity.setProperty("publishedAt", "testPublishedAt");
+
+    Entity articleEntity1 = new Entity("Article");
+    articleEntity1.setProperty("publisher", "testPublisher2");
+    articleEntity1.setProperty("author", "testAuthor2");
+    articleEntity1.setProperty("title", "testTitle2");
+    articleEntity1.setProperty("description", "testDescription2");
+    articleEntity1.setProperty("url", "testUrl2");
+    articleEntity1.setProperty("publishedAt", "testPublishedAt2");
+    
+    // Adding two entities to our mock datastore
+    ds.put(articleEntity);
+    ds.put(articleEntity1);
+
+    Query query = new Query("Article");
+    PreparedQuery results = ds.prepare(query);
+
+    // Checking if the datastore has these two entities. 
+    assertEquals(2,results.countEntities());
+
+    // Calling a function, that given a query and datastore, deletes all the entities of that kind from the datastore.
+    getArticlesServlet.deleteResultsOfQueryFromDatastore(query,ds);
+
+    // Size is zero since all entities are deleted.
+    assertEquals(0,ds.prepare(query).countEntities());
   }
 }
