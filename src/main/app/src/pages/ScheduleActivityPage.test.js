9da6a42f84ef4b4ea56396f9f2a3f7990d399f83
mbouadeus <mbouadeus@gmail.com>
2020-08-16 21:16:26 +0000

    Add title form validation test.

--- src/main/app/src/pages/ScheduleActivityPage.test.js
+++ src/main/app/src/pages/ScheduleActivityPage.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import ScheduleActivityPage from '../ScheduleActivityPage';
+import ScheduleActivityPage from './ScheduleActivityPage';
+import { TextField } from '@material-ui/core';
+
+/** Initialize test props for the schedule activity page. */
+const props = {
+  isLoggedIn: true,
+  accessToken: "",
+  userId: "",
+  activity: {},
+  links: [],
+  eventScheduled: "", 
+  updateEventScheduled: () => {},
+  updateActivity: () => {},
+  activityType: "games",
+  isGuest: false,
+  randomActivities: [],
+  updateRandomActivities: () => {}
+}
 
 /** Test form validation */
 
-test('', () => {
+test('Check title error shown when form submitted with an empty title', () => {
+  // Create mock of SchduleActivityPage
+  const scheduleActivityPage = shallow(<ScheduleActivityPage {...props} />);
 
+  // Submit form without setting title
+  scheduleActivityPage.find('#submit-btn').simulate('click');
+
+  // Check for invalid title error
+  expect(scheduleActivityPage.childAt(0).text()).toEqual('ErrorInvalid title. — check it out!');
 });
