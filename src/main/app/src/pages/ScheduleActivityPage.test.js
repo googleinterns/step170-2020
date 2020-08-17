ffd4ee787dcd54c281da10a2c67e5874a1109942
mbouadeus <mbouadeus@gmail.com>
2020-08-17 15:05:30 +0000

    Add event date validation and email validation tests.

--- src/main/app/src/pages/ScheduleActivityPage.test.js
+++ src/main/app/src/pages/ScheduleActivityPage.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import ScheduleActivityPage from './ScheduleActivityPage';
-import { TextField } from '@material-ui/core';
+import { isValidEmail, validate } from '../hooks/formValidation';
 
 /** Initialize test props for the schedule activity page. */
 const props = {
@@ -30,4 +30,42 @@ test('Check title error shown when form submitted with an empty title', () => {
 
   // Check for invalid title error
   expect(scheduleActivityPage.childAt(0).text()).toEqual('ErrorInvalid title. — check it out!');
+});
+
+test('Check date error shown when start time ahead of end time', () => {
+  // Initialize start time ahead of end time
+  const endTime = new Date();
+  const startTime = new Date(endTime.getTime() + 1);
+
+  // Validate with valid title but invalid dates
+  const errors = validate("title", startTime, endTime, () => {}, () => {});
+
+  // Check if date error
+  expect(errors).toEqual(true);
+});
+
+test('Check email validation', () => {
+  // Initialize invalid emails
+  const invalidEmails = ["plainaddress", "#@%^%#$@#$@#.com", "@example.com",
+    "Joe Smith <email@example.com>", "email.example.com", "email@example@example.com",
+    ".email@example.com", "email.@example.com", "email..email@example.com",
+    "email@example.com (Joe Smith)", "email@example", "email@111.222.333.44444",
+    "email@example..com", "Abc..123@example.com"];
+
+  // Initialize valid emails
+  const validEmails = ["email@example.com", "firstname.lastname@example.com",
+    "email@subdomain.example.com", "firstname+lastname@example.com",
+    "\"email\"@example.com","1234567890@example.com", "email@example-one.com", 
+    "_______@example.com", "email@example.name", "email@example.museum", 
+    "email@example.co.jp", "firstname-lastname@example.com"];
+
+  // Check all invalid emails
+  for (const email of invalidEmails) {
+    expect(isValidEmail(email)).toEqual(false);
+  }
+
+  // Check all valid emails
+  for (const email of validEmails) {
+    expect(isValidEmail(email)).toEqual(true);
+  }
 });
