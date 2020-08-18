8fce7287a441d71c217d395a87a28fe42c579a9d
trinitydonohugh <trinitydonohugh@gmail.com>
2020-08-18 19:34:46 +0000

    Removed testData and added unit tests.

--- src/main/app/src/hooks/updateActivityLinks.js
+++ src/main/app/src/hooks/updateActivityLinks.js
@@ -1,6 +1,5 @@
 /* Fetches data from database through web servlets.
    UpdateLinks is a function used to update the data of that activity. And the data is retrieved using this servlet's fetch request to './servlet-url' (dataLink) */
-import { tempData } from '../test.js';
 
 const updateActivityLinks = (updateLinks, servlet) => {
   fetch(servlet)
@@ -9,7 +8,6 @@ const updateActivityLinks = (updateLinks, servlet) => {
     // Modify the links that have the data according to the activity (the servlet that is called).
     updateLinks(data);
   }).catch(err => {
-    if (err == "SyntaxError: Unexpected token < in JSON at position 0") updateLinks(tempData);
     console.log('err', err);
   });
 }
