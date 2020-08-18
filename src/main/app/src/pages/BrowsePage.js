11b66363e81952f5031a4a277b9963aa09624915
bhargaviks <bhargavi.kumar2012@gmail.com>
2020-08-18 17:34:15 +0000

    Browsepage game filter

--- src/main/app/src/pages/BrowsePage.js
+++ src/main/app/src/pages/BrowsePage.js
@@ -8,10 +8,47 @@ import VideoCard from '../constants/VideoCard.js';
 /* Component for browse page */
 const BrowsePage = ({ links, activityType, updateActivityType, updateActivity, updateServlet, articleData, videoData, gameData}) => {
 
+  // State for game filters
+  const [linkFilters, updateLinksFilters] = React.useState({});             // this is the user entered input
+  const [filteredLinks, updateFilteredLinks] = React.useState(links);       // The links or filtered links after user clicks button. 
+
+  const filterButtonClick = evt => {
+    updateFilteredLinks(links.filter(isWithinRange, linkFilters.numOfPlayers));
+  }
+
+  const filterResetClick = evt => {
+    updateLinksFilters(Object.assign(linkFilters, {numOfPlayers: -1}));
+    updateFilteredLinks(links);
+    console.log(linkFilters.numOfPlayers);
+  }
+
+  // This is the function that gets triggered on typing any number in the textfield for GAME filters.
+  const handleGameFilterChange = evt => {
+    updateLinksFilters(Object.assign(linkFilters, {numOfPlayers: evt.target.value}));
+  }
+
   // Update activty selection and web servlet state based on dropdown.
   const handleActivitySelection = evt => {
     updateActivityType(evt.target.value);
     updateServlet(evt.target.value == "active" ? videoData : evt.target.value == "reading" ? articleData : gameData);
+  }
+
+  // const placeholdertext = () => {
+  //   if(linkFilters.numOfPlayers!= undefined && linkFilters.numOfPlayers != -1)
+  //     return "" + linkFilters.numOfPlayers;
+  //   else
+  //     return "How many people are there?";
+  // }
+
+  function isWithinRange(game, input) {
+     
+    if( (game.minPlayer === 0 && game.maxPlayer === 0) ||                 // If there are no restrictions on the number of players
+        (game.minPlayer === 0 && game.maxPlayer >= input) ||              // If no restriciton on minPlayer, but there is a max limit satisfied
+        (game.maxPlayer === 0 && game.minPlayer <= input) ||              // If no restriction on maxPLayer, but minimum limit is satisfied
+        (game.minPlayer <= input && game.maxPlayer >= input)  ) {         // If both are non-zero, and the game players are within limit. 
+      return game;
+    } 
+    // Otherwise don't return. 
   }
 
   return (
@@ -30,10 +67,18 @@ const BrowsePage = ({ links, activityType, updateActivityType, updateActivity, u
           </div>
         </div>
 
+        <div className="field">
+          <div className="control">
+           <p> Filter out games based on the number of players:</p> <textarea onChange={handleGameFilterChange} className="textarea is-primary" placeholder="How many people are there?"></textarea> 
+           <button className="button is-danger" onClick={filterButtonClick}>Filter</button>
+           <button className="button is-info" onClick={filterResetClick} >Reset Filter</button>
+          </div>
+        </div>
+
         <div className="section-padding-large mb-3 mx-5">
           <div className="row">
             <div className="data-container is-fullwidth">
-              {links.map((data, key) => {
+              {filteredLinks.map((data, key) => {
                 if (activityType === "games") {
                   return (
                     <div key={key}>
