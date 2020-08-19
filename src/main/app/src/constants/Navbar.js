68d099fbe56f7ee641e3edc459543f7cbcbc0adb
Bhargavi Kumar Sundaresan <43729861+bhargaviks@users.noreply.github.com>
2020-08-19 15:44:03 -0700

    Update Navbar.js
    collapsed to single line

--- src/main/app/src/constants/Navbar.js
+++ src/main/app/src/constants/Navbar.js
@@ -23,9 +23,7 @@ const Navbar = ({isLoggedIn, updateIsLoggedIn, updateAccessToken, updateUserId, 
         <div className="navbar-brand">
           <NavLink className="navbar-item" to="/"> WeTime <i className="fas fa-heartbeat"></i></NavLink>
           
-          <a onClick={() => {
-              setisActive(!isActive);
-            }} role="button" className={`navbar-burger burger ${isActive ? "is-active" : ""}`} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <a onClick={() => {setisActive(!isActive);}} role="button" className={`navbar-burger burger ${isActive ? "is-active" : ""}`} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
