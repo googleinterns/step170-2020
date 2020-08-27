1323ca14cd571114b270f51f69b4026f96dd713c
Bhargavi Kumar Sundaresan <43729861+bhargaviks@users.noreply.github.com>
2020-08-26 20:46:48 -0700

    Update AboutPage.js

--- src/main/app/src/pages/AboutPage.js
+++ src/main/app/src/pages/AboutPage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import Line from '../constants/Line';
-import '../css/home.css';
 
 /* Component for About page */
 const AboutPage = () => {
